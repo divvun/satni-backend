b85e93bca1db366e8fc300a793d8468d6b9345f3
Børre Gaup <albbas@gmail.com>
2021-03-25 15:03:43 +0100

    No mutations as of now, no lemma queries

--- backend/schema.py
+++ backend/schema.py
@@ -1,21 +1,19 @@
 import graphene
 
 import dicts.schema
-import lemmas.schema
-import lemmas.types
 import stems.schema
 import terms.schema
 
 
 class Query(stems.schema.Query, dicts.schema.Query, terms.schema.Query,
-            lemmas.schema.Query, graphene.ObjectType):
+            graphene.ObjectType):
     pass
 
 
-class Mutation(lemmas.schema.Mutations, graphene.ObjectType):
-    pass
-
+# class Mutation(graphene.ObjectType):
+#     pass
+#
 
 schema = graphene.Schema(query=Query,
-                         mutation=Mutation,
+                         # mutation=Mutation,
                          types=[stems.types.StemType, terms.types.ConceptType])
