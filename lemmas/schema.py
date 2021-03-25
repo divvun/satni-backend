96ea24a4ec74f6fdbcec6adc21d5d3edc30d2fda
Børre Gaup <albbas@gmail.com>
2021-03-25 14:31:45 +0100

    Don't do queries and mutations on Lemmas

--- lemmas/schema.py
+++ lemmas/schema.py
@@ -7,19 +7,17 @@ from .mutations import (CreateLemmaMutation, DeleteLemmaMutation,
                         UpdateLemmaMutation)
 from .types import LemmaType
 
+# class Query(graphene.ObjectType):
+#     lemma_list = MongoengineConnectionField(LemmaType,
+#                                             search=graphene.String())
+#
+#     def resolve_lemma_list(self, info, search=None, **kwargs):
+#         if search:
+#             return Lemma.objects.filter(lemma__istartswith=search)
+#
+#         return Lemma.objects.all()
 
-class Query(graphene.ObjectType):
-    lemma_list = MongoengineConnectionField(LemmaType,
-                                            search=graphene.String())
-
-    def resolve_lemma_list(self, info, search=None, **kwargs):
-        if search:
-            return Lemma.objects.filter(lemma__istartswith=search)
-
-        return Lemma.objects.all()
-
-
-class Mutations(graphene.ObjectType):
-    create_lemma = CreateLemmaMutation.Field()
-    update_lemma = UpdateLemmaMutation.Field()
-    delete_lemma = DeleteLemmaMutation.Field()
+# class Mutations(graphene.ObjectType):
+#     create_lemma = CreateLemmaMutation.Field()
+#     update_lemma = UpdateLemmaMutation.Field()
+#     delete_lemma = DeleteLemmaMutation.Field()
