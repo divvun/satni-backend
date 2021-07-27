438282ed094b9185325bef7e78cbd49fb54352fe
Børre Gaup <albbas@gmail.com>
2021-07-27 16:55:14 +0200

    Check if stems of pos are generatable

--- generator/test_generator.py
+++ generator/test_generator.py
@@ -14,21 +14,25 @@ class TestParadigmGenerator(unittest.TestCase):
         self.generator = ParadigmGenerator("sme")
 
     @params(
-        ("guolli", "N+Sg+Nom"),
-        ("vuolgit", "V+IV+Inf"),
-        ("vielgat", "A+Sg+Nom"),
+        ("guolli", "N"),
+        ("vuolgit", "V"),
+        ("vielgat", "A"),
         (
             "sihkarvuođaeiseváldi+v3",
-            "N+Sg+Nom",
+            "N",
         ),  # +v3 form is a result from find_best_analysis
         (
             "gielda+N+Cmp/SgNom+Cmp#viessu",
-            "N+Sg+Nom",
+            "N",
+        ),  # the compound is returned from the find_best_analysis
+        (
+            "olgoriika+N+Cmp/SgNom+Cmp#ášši",
+            "N",
         ),  # the compound is returned from the find_best_analysis
     )
-    def test_generate(self, word, paradigm_template):
+    def test_generate(self, word, partOfSpeech):
         """Test that the engine itself works as expected."""
-        assert list(self.generator.generate(word, paradigm_template)) != []
+        assert list(self.generator.generate_wordforms(word, partOfSpeech)) != []
 
     @params(
         ("sihkkarvuođaeiseváldi", "N", "sihkarvuođaeiseváldi+v3"),
