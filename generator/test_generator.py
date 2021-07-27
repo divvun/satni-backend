850833cdc88e6505f00c32e7158d262d43192233
Børre Gaup <albbas@gmail.com>
2021-07-27 23:17:58 +0200

    Add a failing south sami word to the tests

--- generator/test_generator.py
+++ generator/test_generator.py
@@ -11,49 +11,62 @@ class TestParadigmGenerator(unittest.TestCase):
     """Test the ParadigmGenerator class."""
 
     def setUp(self):
-        self.generator = ParadigmGenerator("sme")
+        self.generators = {
+            language: ParadigmGenerator(language) for language in ["sme", "sma"]
+        }
 
     @params(
-        ("guolli", "N"),
-        ("vuolgit", "V"),
-        ("vielgat", "A"),
+        ("sme", "guolli", "N"),
+        ("sme", "vuolgit", "V"),
+        ("sme", "vielgat", "A"),
         (
+            "sme",
             "sihkarvuođaeiseváldi+v3",
             "N",
         ),  # +v3 form is a result from find_best_analysis
         (
+            "sme",
             "gielda+N+Cmp/SgNom+Cmp#viessu",
             "N",
         ),  # the compound is returned from the find_best_analysis
         (
+            "sme",
             "olgoriika+N+Cmp/SgNom+Cmp#ášši",
             "N",
         ),  # the compound is returned from the find_best_analysis
         (
+            "sme",
             "viellja+N+Der/lasj",
             "A",
         ),  # the derivation is returned from the find_best_analysis
         (
+            "sme",
             "njammat+V+TV+Der/PassS+V+IV+Der/d",
             "V",
         ),
         (
+            "sme",
             "nubbi nubbi",
             "Pron",
         ),
+        ("sma", "Magdiel", "N"),
     )
-    def test_generate(self, word, part_of_speech):
+    def test_generate(self, language, word, part_of_speech):
         """Test that the engine itself works as expected."""
-        assert list(self.generator.generate_wordforms(word, part_of_speech)) != []
+        assert (
+            list(self.generators[language].generate_wordforms(word, part_of_speech))
+            != []
+        )
 
     @params(
-        ("sihkkarvuođaeiseváldi", "N", "sihkarvuođaeiseváldi+v3"),
-        ("gieldaviessu", "N", "gielda+N+Cmp/SgNom+Cmp#viessu"),
-        ("olgoriikaášši", "N", "olgoriika+N+Cmp/SgNom+Cmp#ášši"),
-        ("vieljalaš", "A", "viellja+N+Der/lasj"),
-        ("njammot", "V", "njammat+V+TV+Der/PassS+V+IV+Der/d"),
-        ("nuppit nuppiid", "Pron", "nubbi nubbi"),
+        ("sme", "sihkkarvuođaeiseváldi", "N", "sihkarvuođaeiseváldi+v3"),
+        ("sme", "gieldaviessu", "N", "gielda+N+Cmp/SgNom+Cmp#viessu"),
+        ("sme", "olgoriikaášši", "N", "olgoriika+N+Cmp/SgNom+Cmp#ášši"),
+        ("sme", "vieljalaš", "A", "viellja+N+Der/lasj"),
+        ("sme", "njammot", "V", "njammat+V+TV+Der/PassS+V+IV+Der/d"),
+        ("sme", "nuppit nuppiid", "Pron", "nubbi nubbi"),
+        ("sma", "Magdiel", "N", "Magdiel"),
     )
-    def test_find_best_analysis(self, lemma, pos, best_analysis):
+    def test_find_best_analysis(self, language, lemma, pos, best_analysis):
         """Test that the best analysis is found."""
-        assert self.generator.find_best_analysis(lemma, pos) == best_analysis
+        assert self.generators[language].find_best_analysis(lemma, pos) == best_analysis
