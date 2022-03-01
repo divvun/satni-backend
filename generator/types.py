2880558262772b62d0c41c2dc58122b5e6aaadc5
Børre Gaup <albbas@gmail.com>
2022-03-01 22:07:13 +0100

    More things that are required

--- generator/types.py
+++ generator/types.py
@@ -5,12 +5,12 @@ import graphene
 class GeneratorAnalysis(graphene.ObjectType):
     """HFST gives a wordform and a weight."""
 
-    wordform = graphene.String()
-    weight = graphene.Float()
+    wordform = graphene.String(required=True)
+    weight = graphene.Float(required=True)
 
 
 class GeneratorResultType(graphene.ObjectType):
     """The result of the generation of one paradigm template."""
 
-    paradigm_template = graphene.String()
+    paradigm_template = graphene.String(required=True)
     analyses = graphene.List(GeneratorAnalysis)
