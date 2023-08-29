656af9ee6ed26f9a478e1d02c3348e18d1c09602
Børre Gaup <albbas@gmail.com>
2023-08-29 16:24:56 +0200

    cors middleware

--- main/__init__.py
+++ main/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.asgi import GraphQL
 
 from api.schema import schema
@@ -6,4 +7,17 @@ from api.schema import schema
 graphql_app = GraphQL(schema)
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.mount("/graphql", graphql_app)
