6fd3f432ab94f2284fdbfd5960340a36b1c73b7d
Børre Gaup <albbas@gmail.com>
2025-09-24 22:44:58 +0200

    Fix bug caused by breaking changes in graphene-mongo
    MongoengineObjectType requires explicit field definitions for complex relationships like ReferenceField and ListField(ReferenceField)

--- dicts/types.py
+++ dicts/types.py
@@ -1,6 +1,15 @@
+import graphene
+from graphene import relay
 from graphene_mongo import MongoengineObjectType
+from graphene_mongo.fields import MongoengineConnectionField
+from lemmas.types import LemmaType
 
 from .models import DictEntry, ExampleGroup, Restriction, TranslationGroup
+
+
+class LemmaConnection(relay.Connection):
+    class Meta:
+        node = LemmaType
 
 
 class ExampleGroupType(MongoengineObjectType):
@@ -16,8 +25,18 @@ class RestrictionType(MongoengineObjectType):
 class TranslationGroupType(MongoengineObjectType):
     class Meta:
         model = TranslationGroup
+    
+    translationLemmas = graphene.ConnectionField(LemmaConnection)
+    
+    def resolve_translationLemmas(self, info):
+        return self.translationLemmas
 
 
 class DictEntryType(MongoengineObjectType):
     class Meta:
         model = DictEntry
+    
+    lookupLemmas = graphene.ConnectionField(LemmaConnection)
+    
+    def resolve_lookupLemmas(self, info):
+        return self.lookupLemmas
--- terms/types.py
+++ terms/types.py
@@ -1,4 +1,6 @@
+import graphene
 from graphene_mongo import MongoengineObjectType
+from lemmas.types import LemmaType
 
 from .models import Concept, Term
 
@@ -6,6 +8,11 @@ from .models import Concept, Term
 class TermType(MongoengineObjectType):
     class Meta:
         model = Term
+    
+    expression = graphene.Field(LemmaType)
+    
+    def resolve_expression(self, info):
+        return self.expression
 
 
 class ConceptType(MongoengineObjectType):
